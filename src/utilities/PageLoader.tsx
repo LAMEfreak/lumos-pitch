16c22e9262a72187b0fdc784f5befb04252ef40c
LAMEfreak <leeweirong@outlook.com>
2024-06-02 02:01:01 +0800

    Fix PageLoader component color

--- src/utilities/PageLoader.tsx
+++ src/utilities/PageLoader.tsx
@@ -1,13 +1,17 @@
 import { ThreeDots } from "react-loader-spinner";
+import { useTheme } from "./ThemeProvider";
 
 const PageLoader = () => {
+  // Code currently doesn't support theme obtained from system!!
+  const { theme } = useTheme();
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center text-center p-8">
       <ThreeDots
         visible={true}
         height="80"
         width="60"
-        color="#fff"
+        color={theme === "dark" ? "#fff" : "#000"}
         radius="8"
         ariaLabel="three-dots-loading"
       />
