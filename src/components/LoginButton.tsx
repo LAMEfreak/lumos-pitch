0e75cbe59cdabf5d09aebcdfd20663363caa6fae
LAMEfreak <leeweirong@outlook.com>
2024-06-27 15:26:25 +0800

    Change button copy

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -21,7 +21,7 @@ const LoginButton = () => {
       onClick={handleLogin}
       className="rounded-md py-3 mt-2 px-28 bg-blue-600 hover:bg-blue-700 transition-all duration-300"
     >
-      Start pitching
+      Get started
     </button>
   );
 };
